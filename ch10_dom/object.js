3741cbe2314a1b95aa7403390ae8a53513cf5511
Mohamed Ahmed Sheikh Mahamed <m.sheikdecotech23@gmail.com>
2026-05-17 10:28:41 +0200

    update files

---
+++ ch10_dom/object.js
@@ -0,0 +1,45 @@
+// let person ={
+//     name: "John",   
+//     age: 30,
+//     city: "New York",       
+//     saysalam: function() {
+//         console.log("Asalamu alaikum")
+             
+//     }
+
+// }
+
+// person.saysalam() 
+
+// person.age = 50
+// console.log(person)
+// console.log(person["name"]) 
+
+// let {city} = person
+// console.log(city)
+
+let vehicle = {
+    brand: "Toyota",
+    model: "Camry",
+    year: 2020,
+    color: "red",
+    // owner: person,
+
+    stop: function () {
+        console.log("The vehicle has stopped")
+    }
+
+}
+console.log(vehicle.stop())
+
+// console.log(person.name) // John
+// console.log(vehicle.make) // Toyota
+
+// let animal = new Object()
+// animal.type = "Dog"
+// animal.breed = "Labrador"
+// animal.age = 5
+// animal.color = "Yellow"
+
+// let animal = []
+// console.log(animal)
---
+++ ch10_dom/script.js
@@ -0,0 +1,5 @@
+// for (let counter = 0; counter < 100; counter++) {
+    let secondH1 = document.createElement("h1")
+secondH1.textContent = "I was ceated by JavaScript"
+document.body.append(secondH1)
+secondH1.style.color = "blue"
